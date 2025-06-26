553dd0c0c6a6e66309651c70f0fde5fde6f7335e
Hyuga Tsukui <gakikiki407@gmail.com>
2025-06-26 20:23:13 +0900

    Add: # plamo-translate-nvim: Add error handling for missing 'plamo-translate' command

--- lua/plamo-translate-nvim/init.lua
+++ lua/plamo-translate-nvim/init.lua
@@ -1,7 +1,20 @@
 local M = {}
 
 function M.setup()
-	require("plamo-translate-nvim.server").setup()
+	local ok, err_or_job = pcall(vim.system, { "plamo-translate", "-v" }, { text = true }, function(job)
+		if job.code == 0 then
+			vim.notify("plamo-translate-nvim is using " .. job.stdout:gsub("\n", ""), vim.log.levels.INFO)
+		else
+			vim.notify(("code: %d, stderr: %s"):format(job.code, job.stderr:gsub("\n", " ")), vim.log.levels.ERROR)
+			return
+		end
+	end)
+
+	if not ok then
+		local err = err_or_job
+		vim.notify(("failed to spawn: %s"):format(err), vim.log.levels.ERROR)
+		return
+	end
 	require("plamo-translate-nvim.server").start()
 	require("plamo-translate-nvim.commands").setup()
 end
